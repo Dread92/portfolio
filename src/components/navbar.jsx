15600cafa402e515e932af23f301337844511bfb
Guillaume Blanc <guillaume.blanc92@hotmail.fr>
2023-09-25 02:24:23 +0300

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,12 @@ function Navbar() {
     <div className='navbar__logo'>
     <img src={logoImage} alt="Logo" />
     </div>
-    <div><h1>TEST</h1></div>
+    <ul className="navbar__links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
   </nav>
   )
 }
