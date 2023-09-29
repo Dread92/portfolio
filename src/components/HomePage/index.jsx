db5caa041f9a28f160797d5c4a59d9dc2fb3d342
Guillaume Blanc <guillaume.blanc92@hotmail.fr>
2023-09-29 15:44:11 +0300

    arranging section and style

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -12,6 +12,18 @@ function HomePage() {
             <h1 className='homePage__button'>PROJECTS</h1>
             <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
          </div>
+         <div className='homePage__secondblock'>
+         <div className='homePage__text'>Hi, i'm Guillaume</div>
+            <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
+            <h1 className='homePage__button'>PROJECTS</h1>
+            <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
+         </div>
+         <div className='homePage__thirdblock'>
+         <div className='homePage__text'>Hi, i'm Guillaume</div>
+            <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
+            <h1 className='homePage__button'>PROJECTS</h1>
+            <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
+         </div>
          </div>
       
     )
