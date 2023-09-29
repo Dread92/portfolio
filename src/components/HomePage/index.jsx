5b8433f7fe5a9f79aa8e8a83001fb698d7620ee3
Guillaume Blanc <guillaume.blanc92@hotmail.fr>
2023-09-29 20:06:52 +0300

    style + scroll animation, organizing blocks

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,35 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './HomePage.scss';
 
-
 function HomePage() {
-    return (
-     
-    <div className='homePage__container'>
-        <div className='homePage__firstblock'>
-   
-  
-            <div className='homePage__text'>Hi, i'm Guillaume</div>
-            <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
-            <h1 className='homePage__button'>PROJECTS</h1>
-            <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
-         </div>
-         <div className='homePage__secondblock'>
-         <div className='homePage__text__secondblock'>Hi, i'm Guillaume</div>
-            <div className='homePage__text__description__secondblock'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
-            <h1 className='homePage__button'>PROJECTS</h1>
-            <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
-         </div>
-         <div className='homePage__thirdblock'>
-         <div className='homePage__text__thirdblock'>Hi, i'm Guillaume</div>
-            <div className='homePage__text__description__thirdblock'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
-            <h1 className='homePage__button'>PROJECTS</h1>
-            <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
-         </div>
-         </div>
-      
-    )
-  }
-  
+  useEffect(() => {
+    // Function to handle the scroll animation
+    const handleScrollAnimation = () => {
+      const animatedElements = document.querySelectorAll('.animate-on-scroll');
+      animatedElements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const isVisible = elementTop - window.innerHeight < 0;
 
-export default HomePage
+        if (isVisible && !element.classList.contains('show')) {
+          element.classList.add('show');
+        }
+      });
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScrollAnimation);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScrollAnimation);
+    };
+  }, []);
+
+  return (
+    <div className='homePage__container'>
+      <div className='homePage__firstblock'>
+        <div className='homePage__text'>Hi, i'm Guillaume</div>
+        <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
+        <h1 className='homePage__button'>PROJECTS</h1>
+        <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
+      </div>
+      <div className='homePage__secondblock'>
+        <div className='homePage__text__secondblock animate-on-scroll'>About Me</div>
+        <div className='homePage__text__description__secondblock animate-on-scroll'>Passionate about computers and new technology since childhood, I decided in 2018 to transition what was once a hobby into a professional career. After spending a few years in the Armed Forces of my native country, I pursued a role as a computer technician for LDLC, France's largest computer retailer. In 2022, I applied for a government-sponsored program to obtain a college degree in web development. I successfully graduated in July 2023, primarily focusing on front-end development. I'm based in Kyiv, and speak fluently French and English, currently learning Ukrainian.</div>
+      </div>
+      <div className='homePage__thirdblock'>
+        <div className='homePage__text__thirdblock animate-on-scroll'>Hi, i'm Guillaume</div>
+        <div className='homePage__text__description__thirdblock animate-on-scroll'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import "./navbar.scss"
-import logoImage from "../../assets/webdev.png" // Adjust the path to your image
-
+import React, { useEffect } from 'react';
+import './navbar.scss';
+import logoImage from '../../assets/webdev.png';
 
 function Navbar() {
+  useEffect(() => {
+    // Add the 'show' class after a delay to trigger the translateY animation
+    setTimeout(() => {
+      const navbar = document.querySelector('.navbar');
+      navbar.classList.add('show');
+    }, 500); // Adjust the delay (in milliseconds) as needed
+  }, []);
+
   return (
     <nav className="navbar">
-    <div className='navbar__logo'>
-    <img src={logoImage} alt="Logo" />
-    <div className="navbar__text">
-        Guillaume Blanc<br/>Web Developer
+      <div className="navbar__logo">
+        <img src={logoImage} alt="Logo" />
+        <div className="navbar__text">
+          Guillaume Blanc
+          <br />
+          Web Developer
+        </div>
       </div>
-    </div>
-    <ul className="navbar__links">
+      <ul className="navbar__links">
         <li><a href="/">Home</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-  </nav>
-  )
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
