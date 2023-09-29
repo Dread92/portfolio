1f9967a022191c51c9b8f51323c3f7bd4b687c08
Guillaume Blanc <guillaume.blanc92@hotmail.fr>
2023-09-29 17:33:18 +0300

    update pp + overall style

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -7,20 +7,22 @@ function HomePage() {
      
     <div className='homePage__container'>
         <div className='homePage__firstblock'>
+   
+  
             <div className='homePage__text'>Hi, i'm Guillaume</div>
             <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
             <h1 className='homePage__button'>PROJECTS</h1>
             <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
          </div>
          <div className='homePage__secondblock'>
-         <div className='homePage__text'>Hi, i'm Guillaume</div>
-            <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
+         <div className='homePage__text__secondblock'>Hi, i'm Guillaume</div>
+            <div className='homePage__text__description__secondblock'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
             <h1 className='homePage__button'>PROJECTS</h1>
             <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
          </div>
          <div className='homePage__thirdblock'>
-         <div className='homePage__text'>Hi, i'm Guillaume</div>
-            <div className='homePage__text__description'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
+         <div className='homePage__text__thirdblock'>Hi, i'm Guillaume</div>
+            <div className='homePage__text__description__thirdblock'>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</div>
             <h1 className='homePage__button'>PROJECTS</h1>
             <i className="fa-sharp fa-solid fa-arrow-down fa-bounce arrowDown"></i>
          </div>
