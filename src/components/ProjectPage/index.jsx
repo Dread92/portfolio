e7921e8a89bee5f138734f2f65fb68b41f92630d
Guillaume Blanc <guillaume.blanc92@hotmail.fr>
2023-10-02 20:56:52 +0300

    project page component

---
+++ src/components/ProjectPage/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ProjectPage.scss';
+
+function ProjectPage() {
+    return (
+      <section className='mainContainer'>
+        <div className='image__wip'>
+          {/* Replace 'image-source.jpg' with the actual image URL */}
+          <img src='src\assets\wip.webp' alt='Project Image' />
+        </div>
+      </section>
+    );
+  }
+  
+
+export default ProjectPage
