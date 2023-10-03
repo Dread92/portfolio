2e8e98c32e2c8d582b0a1953e48cf6bdadd3635d
Guillaume Blanc <guillaume.blanc92@hotmail.fr>
2023-10-03 13:47:25 +0300

    conditional rendering for some links

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './navbar.scss';
 import logoImage from '../../assets/webdev.png';
-import { Link as ScrollLink } from 'react-scroll'; // Rename one of the imports
-import { Link as RouterLink } from 'react-router-dom'; // Rename the other import
-
-
-
+import { Link as ScrollLink } from 'react-scroll';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 
 // Function to convert temperature from Kelvin to Celsius
 const convertKelvinToCelsius = (temperature) => {
@@ -14,28 +11,23 @@ const convertKelvinToCelsius = (temperature) => {
 
 function Navbar() {
   const [weatherData, setWeatherData] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
     // Add the 'show' class after a delay to trigger the translateY animation
     setTimeout(() => {
       const navbar = document.querySelector('.navbar');
       navbar.classList.add('show');
-    }, 500); // Adjust the delay (in milliseconds) as needed
+    }, 500);
 
     // Fetch weather data when the component mounts
     fetchWeatherData();
   }, []);
 
-  // Function to fetch weather data from your API
   const fetchWeatherData = async () => {
     try {
-      // Access the API key from the environment variable
-      const apiKey = import.meta.env.VITE_API_KEY
-      console.log(import.meta.env.VITE_API_KEY)
-
-      // Replace 'YOUR_WEATHER_API_ENDPOINT' with your weather API endpoint
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}`);
-
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}`);
 
       if (!response.ok) {
         throw new Error('Failed to fetch weather data');
@@ -50,75 +42,74 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-       < RouterLink className='projectLink ' to="/">
-      <div className="navbar__logo">
-        <img src={logoImage} alt="Logo" />
-        <div className="navbar__text">
-          Guillaume Blanc
-          <br />
-          Web Developer
+      <RouterLink className="projectLink" to="/">
+        <div className="navbar__logo">
+          <img src={logoImage} alt="Logo" />
+          <div className="navbar__text">
+            Guillaume Blanc
+            <br />
+            Web Developer
+          </div>
         </div>
-      </div>
       </RouterLink>
-      
-      <ul className="navbar__links">
-      < RouterLink className='projectLink ' to="/">
-        <li><a>Home</a></li>
-</RouterLink>
 
-        < RouterLink className='projectLink ' to="/projects">
-        <li><a>Projects</a></li></RouterLink>
-        <li>
-          {/* Use Link from react-scroll for smooth scrolling */}
-          <ScrollLink
-            to="homePage__secondblock"
-            spy={true}
-            smooth={true}
-            duration={500}
-            offset={-70} // Adjust the offset if needed to account for your navbar's height
-          >
-            About
-          </ScrollLink>
-        </li>
-        <li>
-          {/* Use Link from react-scroll for smooth scrolling */}
-          <ScrollLink
-            to="homePage__thirdblock"
-            spy={true}
-            smooth={true}
-            duration={500}
-            offset={-70} // Adjust the offset if needed to account for your navbar's height
-          >
-            Contact
-          </ScrollLink>
-        </li>
-        </ul>
-        {weatherData && (
-          <ul className='navbar__weather'>
+      <ul className="navbar__links">
+        <RouterLink className="projectLink" to="/">
+          <li><a>Home</a></li>
+        </RouterLink>
+        <RouterLink className="projectLink" to="/projects">
+          <li><a>Projects</a></li>
+        </RouterLink>
+        {location.pathname !== '/projects' && ( // Conditionally render ScrollLink components
+          <>
+            <li>
+              {/* Use Link from react-scroll for smooth scrolling */}
+              <ScrollLink
+                to="homePage__secondblock"
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-70}
+              >
+                About
+              </ScrollLink>
+            </li>
+            <li>
+              {/* Use Link from react-scroll for smooth scrolling */}
+              <ScrollLink
+                to="homePage__thirdblock"
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-70}
+              >
+                Contact
+              </ScrollLink>
+            </li>
+          </>
+        )}
+      </ul>
+      {weatherData && (
+        <ul className='navbar__weather'>
           <li className="navbar__weather__list">
             <div>
-              {/* Display city name */}
               <span>City: {weatherData.name}</span>
             </div>
             <div>
-              {/* Display temperature in Celsius */}
               <span>Temperature: {convertKelvinToCelsius(weatherData.main.temp)}°C</span>
             </div>
             <div>
-              {/* Display weather description */}
               <span>Weather: {weatherData.weather[0].description}</span>
             </div>
             <div>
-              {/* Display weather icon */}
               <img
                 src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
                 alt={weatherData.weather[0].description}
               />
             </div>
           </li>
-          </ul>
-        )}
-      
+        </ul>
+      )}
     </nav>
   );
 }
